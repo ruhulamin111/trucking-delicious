6e3bd74ad947cc3861b8ba8c0e8c6185650103ed
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-22 23:53:08 +0600

    food show

--- src/components/UI/FoodCard/FoodCard.js
+++ src/components/UI/FoodCard/FoodCard.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../../../styles/FoodCard.css'
 
-const FoodCard = () => {
-    return (
-        <div>
+const FoodCard = ({ item }) => {
+    const { id, title, price, image01 } = item;
 
+    return (
+        <div className='food_card_item'>
+            <div className='food_card_item_img'>
+                <img src={image01} alt="" />
+            </div>
+            <div className='food_card_item_title'>
+                <h5><Link to={`/foods/${id}`} >{title}</Link></h5>
+            </div>
+            <div className='food_card_item_price'>
+                <span>$ {price}</span>
+                <button className='home_button'>Add to cart</button>
+            </div>
         </div>
     )
 }
--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import '../styles/AllFoods.css'
+import category1 from '../assets/images/hamburger.png'
+import category2 from '../assets/images/pizza.png'
+import category3 from '../assets/images/bread.png'
+import { ProductionQuantityLimits } from '@mui/icons-material'
+import data from '../assets/fake-data/products'
+import FoodCard from '../components/UI/FoodCard/FoodCard'
 
 const AllFoods = () => {
     return (
         <div>
             <div className='food_list'>
-
+                <button className='active_food'>All</button>
+                <button className='food_list_item'>
+                    <img src={category1} alt="" />
+                    Burger
+                </button>
+                <button className='food_list_item'>
+                    <img src={category2} alt="" />
+                    Pizza
+                </button>
+                <button className='food_list_item'>
+                    <img src={category3} alt="" />
+                    Bread
+                </button>
+            </div>
+            <div className='all_food_section'>
+                {
+                    data.map((item, i) =>
+                        <FoodCard item={item} />
+                    )
+                }
             </div>
 
         </div>
