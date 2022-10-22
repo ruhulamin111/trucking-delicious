0b6121e9d369d3c74540839602420c6da6c8bc3e
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-22 15:41:08 +0600

    dne

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,48 +1,68 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/images/res-logo.png'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import SendIcon from '@mui/icons-material/Send';
 
 const Footer = () => {
+    const year = new Date().getFullYear()
+
     return (
-        <footer className="footer text-base-content border-t-2 py-5 my-5">
-            <div>
-                <img src={logo} alt="" className='h-20 mb-4' />
-                <p>Trucking Delicious Resturant<br />
-                    Providing delicious food with reasonable prize <br />
-                    Since 2020</p>
-            </div>
-            <div>
-                <span className="footer-title">Delivery Time</span>
-                <span >Sunday - Thursday</span>
-                <span >10:00 AM - 10:00 PM </span>
-                <span >Friday - Saturday </span>
-                <span >Weekend</span>
-            </div>
-            <div>
-                <span className="footer-title">Contact:</span>
-                <span>Location:</span>
-                <span>
-                    Dhanmondi,Dhaka, Bangladesh <br />
-                    Mirpur,Dhaka, Bangladesh <br />
-                </span>
-                <span>
-                    +8801725262423 <br />
-                    trucking-delicious@gmail.com <br />
-                    trucking-delicious.com <br />
-                </span>
-            </div>
-            <div>
-                <span className="footer-title">Newsletter</span>
-                <div className="form-control w-80">
-                    <label className="label">
-                        <span className="label-text">Enter your email address</span>
-                    </label>
-                    <div className="relative">
-                        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
-                        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+        <div>
+            <footer className="footer text-base-content border-t-2 py-5 mt-5">
+                <div>
+                    <img src={logo} alt="" className='h-20 mb-4' />
+                    <p>Trucking Delicious Resturant<br />
+                        Providing delicious food with reasonable prize <br />
+                        Since 2020</p>
+                </div>
+                <div>
+                    <span className="footer-title">Delivery Time</span>
+                    <span >Sunday - Thursday</span>
+                    <span >10:00 AM - 10:00 PM </span>
+                    <span >Friday - Saturday </span>
+                    <span >Weekend</span>
+                </div>
+                <div>
+                    <span className="footer-title">Contact:</span>
+                    <span>Location:</span>
+                    <span>Dhaka, Bangladesh</span>
+                    <span> +8801725262423 <br />
+                        trucking-delicious@gmail.com <br />
+                        trucking-delicious.com <br /> </span>
+
+                </div>
+                <div>
+                    <span className="footer-title">Newsletter</span>
+                    <div className="form-control w-80">
+                        <label className="label">
+                            <span className="label-text">Enter your email address</span>
+                        </label>
+                        <div className="relative">
+                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
+                            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none ">Subscribe  <span className='ml-2'> <SendIcon /></span></button>
+                        </div>
                     </div>
                 </div>
+
+            </footer>
+            <div className='flex justify-between'>
+                <div>
+                    <p>&copy; Copyright {year}, Trucking Delicious All right reserved</p>
+                </div>
+                <div>
+                    <span className='flex gap-4'>
+                        <Link to='/'><YouTubeIcon /></Link>
+                        <Link to='/'><FacebookIcon /></Link>
+                        <Link to='/'><LinkedInIcon /></Link>
+                        <Link to='/'><InstagramIcon /></Link>
+                    </span>
+                </div>
             </div>
-        </footer>
+        </div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Helmet from '../components/Helmet/Helmet'
+import logo from '../assets/images/hero.png'
 
 const Home = () => {
+
     return (
-        <div>Home</div>
+        <Helmet title='Home'>
+            <div className='flex justify-between items-center'>
+                <div className=''>
+                    <h1 className='text-xl mt-5'>Easy way to make an order</h1>
+                    <h1 className='text-4xl font-bold mt-5 '>Hungry! just wait food at your home</h1>
+                    <p className='mt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui expedita reiciendis quasi distinctio voluptatem placeat doloribus dolores repellat esse quis.</p>
+                </div>
+                <div>
+
+                    <img className='' src={logo} alt="" />
+                </div>
+
+            </div>
+        </Helmet>
     )
 }
 
