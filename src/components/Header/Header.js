76ebaf82b7ab68a5d3c3f2f5ca2767f7004278a0
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-22 09:25:25 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import logo from '../../assets/images/res-logo.png'
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar font-semibold text-lg h-20 border-b-2">
                 {/* small screen */}
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -15,36 +16,27 @@ const Header = () => {
 
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li tabIndex={0}>
-                                <a className="justify-between">
-                                    Parent
-                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            <li ><Link to='/' className='hover:bg-red-200 ' >Home </Link></li>
+                            <li ><Link to='/' className='hover:bg-red-200'>Foods </Link></li>
+                            <li ><Link to='/' className='hover:bg-red-200'>Cart</Link></li>
+                            <li ><Link to='/' className='hover:bg-red-200'>Contact </Link></li>
                         </ul>
                     </div>
                     <img src={logo} alt="" className='h-12' />
-                    <a href='/' className="btn btn-ghost font-bold normal-case text-2xl">Trucking Delicious</a>
+                    <Link to='/' className="btn btn-ghost font-extrabold normal-case text-2xl">Trucking Delicious</Link>
                 </div>
                 {/* large screen */}
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a>Item </a></li>
-                        <li><a>Item </a></li>
-                        <li><a>Item </a></li>
-                        <li><a>Item </a></li>
-                        <li><a>Item 3</a></li>
+                        <li ><Link to='/' className='hover:bg-red-200 ' >Home </Link></li>
+                        <li ><Link to='/' className='hover:bg-red-200'>Foods </Link></li>
+                        <li ><Link to='/' className='hover:bg-red-200'>Cart</Link></li>
+                        <li ><Link to='/' className='hover:bg-red-200'>Contact </Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a href='/' className="btn btn-ghost font-bold normal-case text-2xl"><ShoppingBasketIcon /></a>
-                    <a href='/' className="btn btn-ghost font-bold normal-case text-2xl"><AccountCircleIcon /></a>
+                    <Link to='/' className="btn btn-ghost font-bold normal-case text-2xl"><ShoppingBasketIcon /> <sup >0</sup></Link>
+                    <Link to='/' className="btn btn-ghost font-bold normal-case text-2xl"><AccountCircleIcon /></Link>
                 </div>
             </div>
         </div>
