2873d445e4a947728306ce0cac8add3b35bfe7f1
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-24 20:46:22 +0600

    done

--- src/components/UI/Cart/CartItem.js
+++ src/components/UI/Cart/CartItem.js
@@ -2,8 +2,28 @@ import React from 'react'
 import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../../store/shoppingCart/cartSlice';
 
 const CartItem = ({ item }) => {
+    const { id, title, price, image01 } = item
+    const disPatch = useDispatch()
+
+    const incrementItem = () => {
+        disPatch(cartActions.addItem({
+            id,
+            title,
+            price,
+            image01,
+        }))
+    }
+    const decrementItem = () => {
+        disPatch(cartActions.removeItem(id))
+    }
+    const deleteItem = () => {
+        disPatch(cartActions.deleteItem(id))
+    }
+
 
     return (
         <div className='cart_item_info'>
@@ -16,12 +36,12 @@ const CartItem = ({ item }) => {
                         <p className='text-[#ff4d6d] font-semibold'>$ {item.totalPrice}</p>
                     </div>
                     <div className='bg-[#ff4d9d] text-white rounded flex justify-between p-1 my-2'>
-                        <span><AddIcon /></span>
+                        <span onClick={incrementItem}><AddIcon /></span>
                         <span>{item.quantity}</span>
-                        <span><RemoveIcon /></span>
+                        <span onClick={decrementItem}><RemoveIcon /></span>
                     </div>
                 </div>
-                <span><CloseIcon /></span>
+                <span onClick={deleteItem}><CloseIcon /></span>
             </div>
         </div>
     )
--- src/store/shoppingCart/cartSlice.js
+++ src/store/shoppingCart/cartSlice.js
@@ -14,6 +14,7 @@ const cartSlice = createSlice({
             const newItem = action.payload
             const existingItem = state.cartItems.find(item => item.id === newItem.id)
             state.totalQuantity++
+
             if (!existingItem) {
                 state.cartItems.push({
                     id: newItem.id,
@@ -26,11 +27,42 @@ const cartSlice = createSlice({
             } else {
                 existingItem.quantity++
                 existingItem.totalPrice = Number(existingItem.totalPrice) + Number(newItem.price)
-                state.totalAmount = state.cartItems.reducer((total, item) => (
-                    total + Number(item.price) * Number(item.quantity)
-                ))
             }
+            state.totalAmount = state.cartItems.reduce((total, item) => (
+                total + Number(item.price) * Number(item.quantity)
+            ))
+        },
+
+        removeItem(state, action) {
+            const id = action.payload
+            const existingItem = state.cartItems.find(item => item.id === id)
+            state.totalQuantity--
+
+            if (existingItem.quantity === 1) {
+                state.cartItems = state.cartItems.filter(item => item.id !== id)
+            } else {
+                existingItem.quantity--
+                existingItem.totalPrice = Number(existingItem.totalPrice) - Number(existingItem.price)
+            }
+            state.totalAmount = state.cartItems.reduce((total, item) => (
+                total + Number(item.price) * Number(item.quantity)
+            ), 0)
+        },
+
+        deleteItem(state, action) {
+            const id = action.payload
+            const existingItem = state.cartItems.find(item => item.id === id)
+
+            if (existingItem) {
+                state.cartItems = state.cartItems.filter(item => item.id !== id)
+                state.totalQuantity = state.totalQuantity - existingItem.totalQuantity
+            }
+            state.totalAmount = state.cartItems.reduce((total, item) => (
+                total + Number(item.price) * Number(item.quantity)
+            ), 0)
         }
+
+
     }
 })
 
