1dd4e65bf49a297147908532f7671cb04f10472e
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-25 12:11:29 +0600

    related product show

--- src/components/UI/CommonSection/CommonSection.js
+++ src/components/UI/CommonSection/CommonSection.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import '../../../styles/CommonSection.css'
 
-const CommonSection = () => {
+const CommonSection = ({ title }) => {
     return (
         <section className='common_section'>
-            <h1 className='text-white font-bold text-2xl ml-16'>All Foods</h1>
+            <h1 className='text-white font-bold text-2xl ml-16'>{title}</h1>
         </section>
     )
 }
--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -5,26 +5,40 @@ import img1 from '../assets/images/product_01.1.jpg'
 import '../styles/FoodDetails.css'
 import { useParams } from 'react-router-dom'
 import products from '../assets/fake-data/products'
+import { useDispatch } from 'react-redux'
+import { cartActions } from '../store/shoppingCart/cartSlice'
+import FoodCard from '../components/UI/FoodCard/FoodCard'
 
 const FoodDetails = () => {
     const [tab, setTab] = useState('desc')
     const { id } = useParams()
     const product = products.find(product => product.id === id)
+    const { title, price, desc, category, image01 } = product;
     const [previewImg, setPreviewImg] = useState(product.image01)
+    const relatedProduct = products.filter(item => category === item.category)
+    const disPatch = useDispatch()
+    const addToCart = () => {
+        disPatch(cartActions.addItem({
+            id,
+            title,
+            image01,
+            price
+        }))
+    }
 
     return (
         <Helmet title='Food Details'>
-            <CommonSection />
-            <section className=''>
+            <CommonSection title={title} />
+            <section className='food_section'>
                 <div className='food_details_section'>
                     <div className="product_img">
                         <div className="img_item" onClick={() => setPreviewImg(product.image01)}>
                             <img src={product.image01} alt="" />
                         </div>
-                        <div className="img_item" onClick={() => setPreviewImg(product.image01)}>
+                        <div className="img_item" onClick={() => setPreviewImg(product.image02)}>
                             <img src={product.image02} alt="" />
                         </div>
-                        <div className="img_item" onClick={() => setPreviewImg(product.image01)}>
+                        <div className="img_item" onClick={() => setPreviewImg(product.image03)}>
                             <img src={product.image03} alt="" />
                         </div>
                     </div>
@@ -32,50 +46,63 @@ const FoodDetails = () => {
                         <img src={previewImg} alt="" />
                     </div>
                     <div className='single_product_content'>
-                        <h1 className="product_title">Pizza with mashroom</h1>
+                        <h1 className="product_title">{title}</h1>
                         <div className='product_price'>
                             <p>Price: </p>
-                            <span>$123</span>
+                            <span>$ {price}</span>
                         </div>
                         <div className="product_price">
                             <p>Category: </p>
-                            <span>Burger</span>
+                            <span>{category}</span>
                         </div>
-                        <button className='home_button'>Add to cart</button>
+                        <button className='home_button' onClick={addToCart}>Add to cart</button>
                     </div>
                 </div>
                 <div className="product_desc">
                     <div className="tab">
-                        <h5 className='active_tab'>Description</h5>
-                        <h5>Reviews</h5>
+                        <h5 className={`${tab === 'desc' && 'active_tab'}`} onClick={() => setTab('desc')}>Description</h5>
+                        <h5 className={`${tab === 'rev' && 'active_tab'}`} onClick={() => setTab('rev')}>Reviews</h5>
                     </div>
-                    <div className="tab_content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, quam porro minus iste, nihil maxime hic tenetur maiores voluptatibus commodi amet nam magni unde animi quis est? A aliquid, omnis natus voluptate quo, veniam officiis voluptatem molestiae iste tempora, nostrum magnam maiores assumenda incidunt dignissimos ipsam laudantium provident quas. Possimus!</p>
+                    {tab === 'desc' ?
+                        <div className="tab_content">
+                            <p>{desc}</p>
+                        </div> : <>
+                            <div className="review">
+                                <p className='review_name'>John Doe</p>
+                                <p className='review_mail'>abc@gmail.com</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, iste.</p>
+                            </div>
+                            <div className="review">
+                                <p className='review_name'>John Doe</p>
+                                <p className='review_mail'>abc@gmail.com</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, iste.</p>
+                            </div>
+                            <div className="form">
+                                <form                    >
+                                    <div className="form_group">
+                                        <input type="text" name="" id="" placeholder='Enter your name' />
+                                    </div>
+                                    <div className="form_group">
+                                        <input type="email" name="" id="" placeholder='Enter your email' />
+                                    </div>
+                                    <div className="form_group">
+                                        <textarea type="text" name="" id="" placeholder='Enter your comments' />
+                                    </div>
+                                    <input className='message_button' type="submit" value="Send message" />
+                                </form>
+                            </div>
+                        </>
+                    }
+                </div>
+                <div>
+                    <h1 className='related_title'>You might also like</h1>
+                    <div className='all_food_section'>
+                        {
+                            relatedProduct.map((item, i) =>
+                                <FoodCard key={i} item={item} />
+                            )
+                        }
                     </div>
-                </div>
-                <div className="review">
-                    <p className='review_name'>John Doe</p>
-                    <p className='review_mail'>abc@gmail.com</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, iste.</p>
-                </div>
-                <div className="review">
-                    <p className='review_name'>John Doe</p>
-                    <p className='review_mail'>abc@gmail.com</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, iste.</p>
-                </div>
-                <div className="form">
-                    <form                    >
-                        <div className="form_group">
-                            <input type="text" name="" id="" placeholder='Enter your name' />
-                        </div>
-                        <div className="form_group">
-                            <input type="email" name="" id="" placeholder='Enter your email' />
-                        </div>
-                        <div className="form_group">
-                            <textarea type="text" name="" id="" placeholder='Enter your comments' />
-                        </div>
-                        <input className='message_button' type="submit" value="Send message" />
-                    </form>
                 </div>
 
             </section>
