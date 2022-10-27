5d375c088d73e4e131be0e57b02fa68131d84b26
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-27 15:54:59 +0600

    signin

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Helmet from '../components/Helmet/Helmet'
 import CommonSection from '../components/UI/CommonSection/CommonSection'
 import '../styles/Checkout.css'
 
 const Checkout = () => {
+    const shippingInfo = []
     const handleSubmit = (event) => {
         event.preventDefault()
+        const userInfo = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            phone: event.target.phone.value,
+            country: event.target.country.value,
+            code: event.target.code.value,
+            city: event.target.city.value,
+        }
+        shippingInfo.push(userInfo)
     }
+
+    const totalAmount = useSelector(state => state.cart.totalAmount)
+    const shippingCost = 50;
+    const grandTotal = totalAmount + Number(shippingCost)
 
     return (
         <Helmet title='Checkout'>
@@ -16,27 +31,31 @@ const Checkout = () => {
                     <h1 className='checkout_title'>Shipping Information</h1>
                     <form onSubmit={handleSubmit}>
                         <div className="form_checkout">
-                            <input type="text" placeholder='Enter your name' required />
+                            <input name='name' type="text" placeholder='Enter your name' required />
                         </div>
                         <div className="form_checkout">
-                            <input type="email" placeholder='Enter your email' required />
+                            <input name='email' type="email" placeholder='Enter your email' required />
                         </div>
                         <div className="form_checkout">
-                            <input type="number" placeholder='Phone number' required />
+                            <input name='phone' type="number" placeholder='Phone number' required />
                         </div>
                         <div className="form_checkout">
-                            <input type="text" placeholder='Country' required />
+                            <input name='country' type="text" placeholder='Country' required />
                         </div>
                         <div className="form_checkout">
-                            <input type="text" placeholder='City' required />
+                            <input name='city' type="text" placeholder='City' required />
                         </div>
                         <div className="form_checkout">
-                            <input type="text" placeholder='Postal code' required />
+                            <input name='code' type="text" placeholder='Postal code' required />
                         </div>
                         <input className='home_button' type="submit" value="Submit" />
                     </form>
                 </div>
-
+                <div className='checkout_bill'>
+                    <h2>Shipping: $ <span>50</span></h2>
+                    <h2>Sub total : $ <span>{totalAmount}</span></h2>
+                    <h2>Grand total : $ <span>{grandTotal}</span></h2>
+                </div>
             </div>
         </Helmet>
     )
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/UI/CommonSection/CommonSection'
+import '../styles/Signin.css'
 
 const Signin = () => {
+    const shippingInfo = []
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const userInfo = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            phone: event.target.phone.value,
+            country: event.target.country.value,
+            code: event.target.code.value,
+            city: event.target.city.value,
+        }
+        shippingInfo.push(userInfo)
+    }
+
     return (
-        <div>Signin</div>
+        <Helmet title='Sign in'>
+            <CommonSection />
+            <section className="singin_section">
+                <h1 className='signin_title'>Sign in</h1>
+                <div className="singin_part">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form_signin">
+                            <input name='name' type="text" placeholder='Enter your name' required />
+                        </div>
+                        <div className="form_signin">
+                            <input name='email' type="email" placeholder='Enter your email' required />
+                        </div>
+                        <input className='home_button' type="submit" value="Submit" />
+                    </form>
+                </div>
+                <h1>Don't have an account? <Link to='/singup'>Sing up</Link></h1>
+            </section>
+        </Helmet>
     )
 }
 
