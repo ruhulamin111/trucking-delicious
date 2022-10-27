a8cb515e886586752ad658ab9ba1fdea89224668
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-27 11:57:42 +0600

    cart  list

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import Helmet from '../components/Helmet/Helmet'
 import CommonSection from '../components/UI/CommonSection/CommonSection'
 import '../styles/Cart.css'
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useDispatch, useSelector } from 'react-redux';
+import { cartActions } from '../store/shoppingCart/cartSlice';
 
 
 const Cart = () => {
-
+    const cartItems = useSelector(state => state.cart.cartItems)
+    const totalAmount = useSelector(state => state.cart.totalAmount)
+    console.log('totalamoun', totalAmount)
+    const disPatch = useDispatch()
+    const deleteItem = (id) => {
+        disPatch(cartActions.deleteItem(id))
+    }
 
     return (
         <Helmet title='Cart'>
             <CommonSection title='Your Cart' />
             <section className="cart_section">
                 <section className="cart_table">
-                    <table className=''>
+                    <table>
                         <thead>
                             <tr>
                                 <th>Image</th>
@@ -23,16 +32,21 @@ const Cart = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                            {
+                                cartItems.length === 0 ? <h5>No item available</h5> : cartItems.map(item => <tr key={item.id}>
+                                    <td><img className='h-12' src={item.image01} alt="" /></td>
+                                    <td>{item.title}</td>
+                                    <td>$ {item.price}</td>
+                                    <td>{item.quantity}</td>
+                                    <td><span onClick={() => deleteItem(item.id)}><DeleteIcon /></span></td>
+                                </tr>)
+                            }
                         </tbody>
                     </table>
                 </section>
+                <div>
+                    <h1>Subtotal: $ <span>{totalAmount}</span></h1>
+                </div>
             </section>
         </Helmet>
     )
--- src/store/shoppingCart/cartSlice.js
+++ src/store/shoppingCart/cartSlice.js
@@ -24,10 +24,13 @@ const cartSlice = createSlice({
                     quantity: 1,
                     totalPrice: newItem.price
                 })
-            } else {
+            }
+
+            else {
                 existingItem.quantity++
                 existingItem.totalPrice = Number(existingItem.totalPrice) + Number(newItem.price)
             }
+
             state.totalAmount = state.cartItems.reduce((total, item) => (
                 total + Number(item.price) * Number(item.quantity)
             ))
